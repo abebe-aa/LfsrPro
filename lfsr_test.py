5d5cfe889447af95c1024c66130a04790beb5085
abebe <abebeadigo@gmail.com>
2021-06-28 10:01:33 +0300

    sencode.py

--- lfsr_test.py
+++ lfsr_test.py
@@ -2,9 +2,9 @@ import unittest
 
 import lfsr
 
-
 class testLfsr(unittest.TestCase):
     def test_lfsr(self):
         self.assertEqual(lfsr.lfsr([1, 1, 0, 1, 0, 0, 0, 1],[1, 3]), [0, 1, 1, 0, 1, 0, 0, 0])
+
     def test_lfsr2(self):
-        self.assertNotEqual(lfsr.lfsr([1,1,1,1,1,],[1,0,0]),[1,1,1,1,1,1,])
+        self.assertNotEqual(lfsr.lfsr([1,1,1,1,1,],[1,4]),[1,1,1,1,1,1,])
